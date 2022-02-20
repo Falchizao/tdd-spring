bef668c5212cf35c047f8271e1f6e33f6f73fa67
falchizao <marcelonavarro11md@gmail.com>
2022-02-19 23:11:13 -0300

    uploading first spring structure

---
+++ src/main/java/com/example/testdrivendevelopment/TestdrivendevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdrivendevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdrivendevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestdrivendevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testdrivendevelopment/TestdrivendevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdrivendevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdrivendevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
